25c1b98caa7cc0019d91a080ec4e4ed0f540ca6f
kferna34 <kferna34@my.centennialcollege.ca>
2016-05-17 12:42:53 -0400

    initial

--- week02-01/Program.cs
+++ week02-01/Program.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+* Author: Krytia Fernandez
+* Date : May 17, 2016
+*Description: Advance Methods Demo for Lesson 2
+* Version : 0.0.1--  Initial Version 
+*/
 namespace week02_01
 {
     class Program

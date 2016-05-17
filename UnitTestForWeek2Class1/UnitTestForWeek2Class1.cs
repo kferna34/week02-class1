0721ad390274d3bcb2f10cb563b91624ed7849fb
kferna34 <kferna34@my.centennialcollege.ca>
2016-05-17 12:54:04 -0400

    added a ref to driver class from U-Test

--- UnitTestForWeek2Class1/UnitTestForWeek2Class1.cs
+++ UnitTestForWeek2Class1/UnitTestForWeek2Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using week02_01;
 
 namespace UnitTestForWeek2Class1
 {
@@ -9,6 +10,13 @@ namespace UnitTestForWeek2Class1
         [TestMethod]
         public void TestMethod1()
         {
+            //Arrange
+
+            //Act
+
+
+            //Assert
         }
+
     }
 }
--- week02-01/Program.cs
+++ week02-01/Program.cs
@@ -7,12 +7,21 @@ using System.Threading.Tasks;
 * Author: Krytia Fernandez
 * Date : May 17, 2016
 *Description: Advance Methods Demo for Lesson 2
-* Version : 0.0.1--  Initial Version 
+* Version : 0.0.2--  added a ref to driver class from U-Test
 */
 namespace week02_01
 {
+    /*
+    * Main driver class for lesson 2 
+    * @ class program 
+    */
     class Program
     {
+        /**
+        * Main method for our drive class
+        * @ constructor main
+        *@param {string[]} args
+        */
         static void Main(string[] args)
         {
         }
